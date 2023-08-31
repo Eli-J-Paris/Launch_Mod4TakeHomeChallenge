157b11f338d3c002c857024c0b9276a220e853dd
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-08-31 10:23:57 -0600

    get all products test passing

--- CommerceAPITests/EndpointTests/ProductCrudTests.cs
+++ CommerceAPITests/EndpointTests/ProductCrudTests.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿using CommerceAPI.DataAccess;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommerceAPI.Models;
 
 namespace CommerceAPITests.EndpointTests
 {
-    internal class ProductCrudTests
+    public class ProductCrudTests : IClassFixture<WebApplicationFactory<Program>>
     {
+
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ProductCrudTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        private CommerceApiContext GetDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CommerceApiContext>();
+            optionsBuilder.UseInMemoryDatabase("TestDatabase");
+
+            var context = new CommerceApiContext(optionsBuilder.Options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
+        private string ObjectToJson(object obj)
+        {
+            DefaultContractResolver contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            };
+
+            string json = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            {
+                ContractResolver = contractResolver
+            });
+
+            return json;
+        }
+
+
+        [Fact]
+        public async void GetProducts_ReturnsAllProductsforSpecficMerchant()
+        {
+            var context = GetDbContext();
+            var client = _factory.CreateClient();
+
+            var merchant1 = new Merchant { Name = "Biker Jim's", Category = "Restaurant" };
+            context.Merchants.Add(merchant1);
+            Product product1 = new Product { Name = "FOOD", Description = "desription", Category = "bike food", MerchantId = merchant1.Id };
+            Product product2 = new Product { Name = "burger", Description="desription", Category = "people food", MerchantId = merchant1.Id };
+            List<Product> products = new() { product1, product2 };
+            context.Products.AddRange(products);
+
+            context.SaveChanges();
+
+            var response = await client.GetAsync($"/api/merchants/{merchant1.Id}/products");
+            var content = await response.Content.ReadAsStringAsync();
+
+            string expected = ObjectToJson(products);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(expected, content);
+
+        }
+
     }
 }
